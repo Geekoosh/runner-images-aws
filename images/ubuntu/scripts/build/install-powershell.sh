705815bf971b407ecbd832095d43abec9dcb2564
assafkamil <assaf@wisemansoftware.com>
2024-01-15 13:00:12 +0200

    allow-downgrades for powershell install

--- images/ubuntu/scripts/build/install-powershell.sh
+++ images/ubuntu/scripts/build/install-powershell.sh
@@ -10,4 +10,4 @@ source $HELPER_SCRIPTS/install.sh
 pwsh_version=$(get_toolset_value .pwsh.version)
 
 # Install Powershell
-apt-get install -y powershell=$pwsh_version*
+apt-get install -y --allow-downgrades powershell=$pwsh_version*

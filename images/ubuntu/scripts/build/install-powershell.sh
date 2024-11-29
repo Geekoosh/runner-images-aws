7f663595a0f0a463537d70c2dd3f31b38a42fbe8
assafk <assafkamil@gmail.com>
2024-11-29 16:17:11 +0200

    fix powershell install ubuntu24

--- images/ubuntu/scripts/build/install-powershell.sh
+++ images/ubuntu/scripts/build/install-powershell.sh
@@ -11,4 +11,11 @@ source $HELPER_SCRIPTS/os.sh
 pwsh_version=$(get_toolset_value .pwsh.version)
 
 # Install Powershell
-apt-get install powershell=$pwsh_version*
+if is_ubuntu24; then
+    package_path=$(download_with_retry "https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/powershell-lts_7.4.6-1.deb_amd64.deb")
+    sudo dpkg -i "$package_path"
+    sudo apt-get install -f
+    sudo rm "$package_path"
+else
+    apt-get install powershell=$pwsh_version*
+fi
